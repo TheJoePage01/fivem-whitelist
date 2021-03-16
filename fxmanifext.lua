0411d4c481a0c2f2aad5ea201880632cfce8c022
jjboss1121121 <62721814+jjboss1121121@users.noreply.github.com>
2021-03-15 18:31:14 -0700

    Create fxmanifext.lua

---
+++ fxmanifext.lua
@@ -0,0 +1,13 @@
+----------------------------------------
+--- Discord Whitelist, Made by Joe :D---
+----------------------------------------
+
+fx_version 'bodacious'
+game 'gta5'
+server_only 'yes'
+
+author 'JOE'
+description 'discord whitelist for GoodEnd Roleplay'
+
+server_script 'config.js'
+server_script 'server.js'
